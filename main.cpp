eda4c12ed75ce5dfa7dbf5fedc7a4cfee4282af3
plwu1024 <43206330+plwu1024@users.noreply.github.com>
2022-04-07 12:09:43 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include "PinNames.h"
+#include "ThisThread.h"
 #include "mbed.h"
 
 // Adjust pin name to your board specification.
@@ -12,10 +14,35 @@ PwmOut led(PWM_OUT);
 
 int main()
 {
-    // specify period first
-    led.period(0.05f);      // 0.05 second period
-    led.write(0.50f);      // 50% duty cycle, relative to period
-    //led = 0.5f;          // shorthand for led.write()
-    //led.pulsewidth(2);   // alternative to led.write, set duty cycle time in seconds
-    while (1);
+    while (1){
+        led.period(0.05f);
+        led.write(0.1f);
+        ThisThread::sleep_for(3s);
+
+        led.period(0.05f);
+        led.write(0.5f);
+        ThisThread::sleep_for(3s);
+
+        led.period(0.05f);
+        led.write(0.9f);
+        ThisThread::sleep_for(3s);
+
+        led.period(0.25f);
+        led.write(0.1f);
+        ThisThread::sleep_for(3s);
+
+        led.period(0.25f);
+        led.write(0.5f);
+        ThisThread::sleep_for(3s);
+
+        led.period(0.25f);
+        led.write(0.9f);
+        ThisThread::sleep_for(3s);
+    }
+    // // specify period first
+    // led.period(0.05f);      // 4 second period
+    // led.write(0.50f);      // 50% duty cycle, relative to period
+    // //led = 0.5f;          // shorthand for led.write()
+    // //led.pulsewidth(2);   // alternative to led.write, set duty cycle time in seconds
+    // // while (1);
 }
